81e54c018b241eef043aed24af28cf2ba31ce4cc
Shubham <rkshubham345@gmail.com>
2023-06-15 15:46:04 +0530

    Solution

---
+++ Module1/Day1/qns5.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int bit_operations(int num, int oper_type) {
+    int result = num;
+
+    switch (oper_type) {
+        case 1:
+            
+            result |= 1 << 0;
+            break;
+        case 2:
+            
+            result &= ~(1 << 31);
+            break;
+        case 3:
+            
+            result ^= 1 << 15;
+            break;
+        default:
+            printf("Invalid operation type\n");
+            break;
+    }
+
+    return result;
+}
+
+int main() {
+    int num, oper_type;
+    int result;
+
+    printf("Enter the integer: ");
+    scanf("%d", &num);
+
+    printf("Enter the operation type (1, 2, or 3): ");
+    scanf("%d", &oper_type);
+
+    result = bit_operations(num, oper_type);
+
+    printf("Result: %d\n", result);
+
+    return 0;
+}
